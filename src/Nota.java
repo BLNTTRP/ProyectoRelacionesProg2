dff8eb59c5c6b54247b3942bec84763695fe79c9
Joaquin Cosentino <joacocosentino2003@outlook.com.ar>
2025-04-08 22:47:39 -0300

    Commit: clase Nota creada

---
+++ src/Nota.java
@@ -0,0 +1,50 @@
+import java.util.Date;
+
+public class Nota {
+    private int id;
+    private double valor;
+    private Date fechaExamen;
+    private boolean esRecuperatorio;
+    private Catedra catedra;
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getFechaExamen() {
+        return fechaExamen;
+    }
+
+    public void setFechaExamen(Date fechaExamen) {
+        this.fechaExamen = fechaExamen;
+    }
+
+    public boolean isEsRecuperatorio() {
+        return esRecuperatorio;
+    }
+
+    public void setEsRecuperatorio(boolean esRecuperatorio) {
+        this.esRecuperatorio = esRecuperatorio;
+    }
+
+    public Catedra getCatedra() {
+        return catedra;
+    }
+
+    public void setCatedra(Catedra catedra) {
+        this.catedra = catedra;
+    }
+}
