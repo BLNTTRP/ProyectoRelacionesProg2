2cfcaa113b18663faca6075a46429bac44cb8702
Joaquin Cosentino <joacocosentino2003@outlook.com.ar>
2025-04-08 22:47:08 -0300

    Commit: clase Catedra creada

---
+++ src/Catedra.java
@@ -0,0 +1,21 @@
+public class Catedra {
+    private int codigo;
+    private String denominacion;
+
+    // Getters y setters
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+}
